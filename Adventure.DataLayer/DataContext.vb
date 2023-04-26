5fd28e5e34c31c1bd80ff64569b883d925227f6d
AzozoCode <josephbarrigah@gmail.com>
2023-04-26 05:00:54 +0000

    starter

---
+++ Adventure.DataLayer/DataContext.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Configuration
+Imports Adventure.EntityLayer
+Imports Microsoft.EntityFrameworkCore
+Imports Microsoft.IdentityModel.Protocols
+
+Public Class DataContext
+    Inherits DbContext
+    Sub New()
+        MyBase.New()
+    End Sub
+    Public Overridable Property Products As DbSet(Of Product)
+    Protected Overrides Sub OnModelCreating(ByVal modelBuilder As ModelBuilder)
+        modelBuilder.Entity(Of Product)()
+    End Sub
+
+End Class
---
+++ Adventure.EntityLayer/Product.vb
@@ -0,0 +1,15 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+
+Public Class Product
+
+    <Key>
+    Public Property ProductId As Integer
+    <Required>
+    Public Property Name As String
+    Public Property Catergory As String
+    Public Property Price As Decimal
+    Public Property ExpiryDate As Date
+
+
+End Class
